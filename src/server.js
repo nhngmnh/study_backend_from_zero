9d225396a0a4f40aa46de18b4efe422009a293ec
Minh <nhungocminh2004@gmail.com>
2025-02-19 22:51:21 +0700

    ket noi  mysql2

--- src/server.js
+++ src/server.js
@@ -5,9 +5,17 @@ const router= require('./routes/web');
 const path=require('path');
 const port=process.env.PORT;
 const configViewEngine= require('./config/configViewEngine');
-
+const connection= require('./config/db');
 configViewEngine(app);
 app.use('/', router);
+
+connection.query(
+  'select * from Users',
+  function (err,results,fields) {
+    console.log('results',results);
+    console.log('fields',fields);
+  }
+) 
 app.listen(port,process.env.HOST_NAME, () => {
   console.log(`Example app listening on port ${port}`)
 })
